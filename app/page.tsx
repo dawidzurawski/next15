54c8506ead5db7de4dde33325416d64c864fca46
dawidzurawski <dawidzurawskidrz@gmail.com>
2024-10-28 19:46:33 +0100

    Create user params

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Hello from "./components/hello";
 export default function Home() {
   return (
     <>
-      <h1 className="text-3xl">Welcome to Next.js</h1>;
+      <h1 className="text-3xl">Welcome to Next.js</h1>
       <Hello />
     </>
   );
